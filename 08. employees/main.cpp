da95db201870af864656a3a68e7308d453af455c
ffuis-aljosa <a.sljuka@ffuis.edu.ba>
2017-12-19 09:59:59 +0100

    Dynamic array of employees

--- 08. employees/main.cpp
+++ 08. employees/main.cpp
@@ -5,24 +5,38 @@
 #include "HourlyEmployee.h"
 #include "ContractEmployee.h"
 
+#include <vector>
+
 using namespace std;
 
 int main()
 {
     MonthlyEmployee m("Filip", "Jovanovic", "Vuka Karadzica 38", "065123154",
                       1800, 40, 138);
-
-    m.describe();
-
     HourlyEmployee h("Jovana", "Petrovic", "Vilijama Sekspira 3", "066836102",
                      35, 13);
-
-    h.describe();
-
     ContractEmployee c("Danka", "Boskovic", "Jovan Ducica bb", "062737124",
                        10, 800);
 
-    c.describe();
+    Employee* emp = new HourlyEmployee("Goran", "milosevic", "Kasindo bb",
+                                       "066543891", 40, 23);
+
+    cout << emp->paycheck() << endl;
+
+    cout << m.paycheck() << endl;
+
+    vector<Employee*> employees;
+
+    employees.push_back(&m);
+    employees.push_back(&h);
+    employees.push_back(&c);
+
+    int size = employees.size();
+
+    for (int i = 0; i < size; i++)
+        cout << employees[i]->paycheck() << endl;
+
+    delete emp;
 
     return 0;
 }
